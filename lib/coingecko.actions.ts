de4c67c0720c59bce40ee536ff48c87041b8576b
Rahul NS <rahul9380767545@gmail.com>
2026-09-25 09:52:06 +0530

    fix(api): move env var checks inside fetcher function

--- lib/coingecko.actions.ts
+++ lib/coingecko.actions.ts
@@ -5,14 +5,14 @@ import qs from "query-string";
 const BASE_URL = process.env.COINGECKO_BASE_URL;
 const API_KEY = process.env.COINGECKO_API_KEY;
 
-if (!BASE_URL) throw new Error("Could not get base url");
-if (!API_KEY) throw new Error("Could not get api key");
-
 export async function fetcher<T>(
   endpoint: string,
   params?: QueryParams,
   revalidate = 60
 ): Promise<T> {
+  if (!BASE_URL) throw new Error("Could not get base url");
+  if (!API_KEY) throw new Error("Could not get api key");
+
   const url = qs.stringifyUrl(
     {
       url: `${BASE_URL}/${endpoint}`,
